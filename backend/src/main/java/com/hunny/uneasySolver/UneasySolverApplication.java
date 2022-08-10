73c1827d96bb87628e1d3eb4378a98c31bc789ae
DeveloperHunny <eogns0321@gmail.com>
2022-08-10 10:51:28 +0900

    [backend] Setting Project

---
+++ backend/src/main/java/com/hunny/uneasySolver/UneasySolverApplication.java
@@ -0,0 +1,13 @@
+package com.hunny.uneasySolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UneasySolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UneasySolverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hunny/uneasySolver/UneasySolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunny.uneasySolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UneasySolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
